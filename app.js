0eb2e7af50d067f8f2fc2eb30206308f15f81404
benni maxon <benni.maxon@gmail.com>
2022-10-25 15:13:40 -0700

    added console.log for update button to reflect button click

--- app.js
+++ app.js
@@ -3,3 +3,11 @@ console.log(`nameDisplay`, nameDisplay);
 
 nameDisplay.textContent = 'Alejandra';
 nameDisplay.classList.add('yellow');
+
+const updateBtn = document.getElementById('btn');
+updateBtn.addEventListener('click', () => {
+    console.log('I clicked the button');
+    const nameInput = document.getElementById('name-input');
+    nameDisplay.textContent = nameInput.value;
+    nameInput.value = '';
+});
